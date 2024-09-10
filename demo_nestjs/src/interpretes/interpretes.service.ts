18ea107ee328ebcbdb19a5fee8faf09f0da5d7c6
eld4vd <bejarano.chara.josedavid@usfx.bo>
2024-09-10 18:47:13 -0400

    subiendo actualizacion de repositoio

--- demo_nestjs/src/interpretes/interpretes.service.ts
+++ demo_nestjs/src/interpretes/interpretes.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateInterpreteDto } from './dto/create-interprete.dto';
 import { UpdateInterpreteDto } from './dto/update-interprete.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Interprete } from './entities/interprete.entity';
 
 @Injectable()
 export class InterpretesService {
-  create(createInterpreteDto: CreateInterpreteDto) {
-    return 'This action adds a new interprete';
+  constructor(
+    @InjectRepository(Interprete)
+    private interpretesRepository: Repository<Interprete>,
+  ) {}
+
+  async create(createInterpreteDto: CreateInterpreteDto): Promise<Interprete> {
+    const exist = await this.interpretesRepository.findOneBy({
+      nombre: createInterpreteDto.nombre.trim(),
+      nacionalidad: createInterpreteDto.nacionalidad.trim(),
+    });
+
+    if (exist) throw new ConflictException('El interprete ya existe');
+    const interprete = new Interprete();
+    interprete.nombre = createInterpreteDto.nombre.trim();
+    interprete.nacionalidad = createInterpreteDto.nacionalidad.trim();
+    return this.interpretesRepository.save(interprete);
   }
 
-  findAll() {
-    return `This action returns all interpretes`;
+  // Se modifica el método findAll para que retorne una lista de interpretes
+  async findAll(): Promise<Interprete[]> {
+    return this.interpretesRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} interprete`;
+  async findOne(id: number): Promise<Interprete> {
+    const interprete = await this.interpretesRepository.findOneBy({ id });
+    if (!interprete) throw new NotFoundException('El interprete no existe');
+    return interprete;
   }
 
-  update(id: number, updateInterpreteDto: UpdateInterpreteDto) {
-    return `This action updates a #${id} interprete`;
+  async update(
+    id: number,
+    updateInterpreteDto: UpdateInterpreteDto,
+  ): Promise<Interprete> {
+    const Interprete = await this.findOne(id);
+    const interpreteUpdated = Object.assign(Interprete, updateInterpreteDto);
+    return this.interpretesRepository.save(interpreteUpdated);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} interprete`;
+  async remove(id: number) {
+    const interprete = await this.findOne(id);
+    return this.interpretesRepository.softRemove(interprete); //softRemove para no eliminar el registro de la base de datos
   }
 }
